f1dbd2ed10e9be5289e18a12b2e6f5a1ddbd1801
BanuDanabakiyam <banubala9655@gmail.com>
2024-02-28 13:28:50 +0530

    addLikeButton

--- onlinephotogallery/src/components/gallery/Gallery.js
+++ onlinephotogallery/src/components/gallery/Gallery.js
@@ -26,6 +26,7 @@ export const Gallery = () => {
 
     const [itemImage,setItemImage] = useState(false);
     const [tempImgSrc,setTempImgSrc] = useState('');
+    const [liked,setLiked] = useState([]);
 
     const getImage =(imgsrc) => {
         setTempImgSrc(imgsrc);
@@ -38,6 +39,10 @@ export const Gallery = () => {
 
     const handleLikeButton =(id) => {
         console.log("Liked image");
+        const newLikedImages = [...liked];
+        newLikedImages[id-1] = !newLikedImages[id-1];
+        
+        setLiked(newLikedImages);
     }
 
     return (
@@ -46,9 +51,8 @@ export const Gallery = () => {
             <div className="grid-container">
                 {imageData.map((item,index) => (
                     <div className="pics" key={index} onClick={() => getImage(item.imgsrc)}>
-                        
                         <img src={item.imgsrc} alt={`Image ${item.id}`}  />
-                        <div className="like" onClick={(e) => {e.stopPropagation();
+                        <div className="like" style={{color: liked[item.id-1] ? 'blue' : 'white'}} onClick={(e) => {e.stopPropagation();
                             handleLikeButton(item.id)}}>
                                 <ThumbUpAltIcon/>
                             </div>
